9fad3bb36c49316aceb67e9d327fd7abad765675
Marcin <marcin.p.dev@gmail.com>
2022-12-13 17:06:27 +0100

    Added about contact and home poc pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home"
+import About from "./About"
+import Contact from "./Contact"
 import "./App.css";
-
 import { LanguageProvider } from "./containers/Language";
 import LanguageSelector from "./components/LanguageSelector";
 import Explore from "./components/Explore";
@@ -9,10 +12,14 @@ function App() {
     <LanguageProvider>
       <div className="App">
         <header className="App-header">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+          </Routes>
           <LanguageSelector />
-        <Explore />
+          <Explore />
         </header>
-
       </div>
     </LanguageProvider>
   );
