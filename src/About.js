7355682b96622f54d226962d0b4cbbc6a8475105
Marcin <marcin.p.dev@gmail.com>
2022-12-13 17:13:37 +0100

    Added router poc links

--- src/About.js
+++ src/About.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 function About() {
-    return (
-        <div>
-            <h1>This is the about page</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>This is about page</h1>
+      <Link to="Home">Click to view our home page</Link>
+      <Link to="contact">Click to view our contact page</Link>
+    </div>
+  );
 }
 
-export default About
+export default About;
