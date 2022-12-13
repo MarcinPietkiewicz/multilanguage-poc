e6cbafcde93caf1300028d3ac250b39a0889346b
Marcin <marcin.p.dev@gmail.com>
2022-12-13 17:00:50 +0100

    Added react router dependency

--- src/containers/Language.js
+++ src/containers/Language.js
@@ -29,6 +29,5 @@ export function LanguageProvider({ children }) {
 // get text according to id & current language
 export function Text({ tid }) {
   const languageContext = useContext(LanguageContext);
-
   return languageContext.dictionary[tid] || tid;
 }
